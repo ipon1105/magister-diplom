42f177cd59cb2aeff120a042661df616c90d79da
Ivan Konovalov <my_world1105@mail.ru>
2023-11-17 12:01:04 +0300

    Comments was added to class JobContainer. Change function getFirstJob to popFirstJob.

--- newAlgorithm/Model/Job/JobContainer.cs
+++ newAlgorithm/Model/Job/JobContainer.cs
@@ -3,39 +3,70 @@ using System.Linq;
 
 namespace newAlgorithm.Model
 {
+    /// <summary>
+    /// Данный класс описывает множество заданий, как контейнер
+    /// </summary>
     public class JobContainer
     {
+
+        /// <summary>
+        /// Данный список представляет из себя список заданий, открытый на чтения и скрытый на запись извне. Известный как контейнер
+        /// </summary>
         public List<Job> JobList { get; private set; }
 
-        public int LastPosition { get; private set; } = 0;
+        /// <summary>
+        /// Данная перменная описывает позицию последнего задания в списке, известного как контейнер
+        /// </summary>
+        public int LastPosition { get; private set; }
 
+        /// <summary>
+        /// Данная пустой конструктор создает экземпляет, и инициализирует переменные, данного класса
+        /// </summary>
         public JobContainer()
         {
             JobList = new List<Job>();
+            LastPosition = 0;
         }
 
+        /// <summary>
+        /// Данная функция добавляет переданное задание в контейнер и инициализирует его, как последнее
+        /// </summary>
+        /// <param name="job">Задание, которое добавляется в контейнер</param>
         public void add(Job job)
         {
             JobList.Add(job);
             LastPosition++;
         }
 
+        /// <summary>
+        /// Данная функция выполняет поиск задания, по внтренней позиции задания и возвращает задание из контейнера
+        /// </summary>
+        /// <param name="position">Внутренняя позиция задания, по которому необходимо найти задание в контейнере</param>
+        /// <returns>Задание найденное в списке или null</returns>
         public Job find(int position)
         {
+
+            // Используем итераторы для увеличения производительности
             foreach (Job job in JobList)
             {
                 if (job.Position == position)
                     return job;
             }
 
+            // В случае, если ничего не нашли, возвращаем null
             return null;
         }
 
-        public Job getFirstJob()
+        /// <summary>
+        /// Данная функция возвращает первое задание в контейнере
+        /// </summary>
+        /// <returns></returns>
+        public Job popFirstJob()
         {
             Job first = JobList.First();
             JobList.RemoveAt(0);
 
+            // Возвращаем первое задание
             return first;
         }
     }
