0b239229d250efc16fe74ccb52d9efc469636e35
Ivan Konovalov <my_world1105@mail.ru>
2023-11-17 12:26:49 +0300

    Comments was added to classes RMatrixNode and RMatrix.

--- newAlgorithm/Model/RMatrix/RMatrix.cs
+++ newAlgorithm/Model/RMatrix/RMatrix.cs
@@ -2,23 +2,49 @@
 
 namespace newAlgorithm.Model
 {
+    /// <summary>
+    /// Данный класс описывает матрицу количества данных i-ых типов в j-ых партиях
+    /// </summary>
     public class RMatrix
     {
-        public List<RMatrixNode> rMatrix = new List<RMatrixNode>();
-        public List<int> typePriority = new List<int>();
-        public int lastPosition = 0;
-        public int countType = 0;
 
+        /// <summary>
+        /// Данная переменная содержит в себе список узлов матрицы R
+        /// </summary>
+        public List<RMatrixNode> rMatrix;
+
+        /// <summary>
+        /// Данная переменная содержит в себе приоритеты типов данных
+        /// </summary>
+        public List<int> typePriority;
+
+        /// <summary>
+        /// Данная переменная содержит индекс последнего элемента
+        /// </summary>
+        public int lastPosition;
+
+        /// <summary>
+        /// Данная пермення содержит количество типов данных
+        /// </summary>
+        public int countType;
+
+        /// <summary>
+        /// Данный конструктор определяет матрицу количества данных определённых типов в определённых партий. Принимает количество типов и возвращает экземпляр данного класса
+        /// </summary>
+        /// <param name="countType">Количетсво типов данных матрицы R</param>
         public RMatrix(int countType)
         {
+            rMatrix = new List<RMatrixNode>();
+            typePriority = new List<int>();
+            lastPosition = 0;
             this.countType = countType;
         }
 
         /// <summary>
-        /// Добавляет в матрицу R элемент (нужно для построения следующих уровней)
+        /// Данная функция добавляет в матрицу R элемент необходимый для увеличения матрицы
         /// </summary>
-        /// <param name="type"></param>
-        /// <param name="count"></param>
+        /// <param name="type">Тип данных нового элемента</param>
+        /// <param name="count">Количество данных в новом элементе</param>
         public void AddNode(int type, int count)
         {
             RMatrixNode newNode = new RMatrixNode(type, ++lastPosition, count);
@@ -26,49 +52,50 @@ namespace newAlgorithm.Model
         }
 
         /// <summary>
-        /// Clone RMatrix class
+        /// Данная функция выполняет глубокое копирование данного класса
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Склонированный экземпляр класса RMatrix</returns>
         public RMatrix Clone()
         {
+            // Определяем новую матрицу R
             RMatrix newRMatrix = new RMatrix(countType);
 
+            // Определяем новый список узлов
             List<RMatrixNode> newNodeList = new List<RMatrixNode>();
 
+            // Выполяем глубокое копирования каждого элемента матрица
             foreach (RMatrixNode rMatrixNode in rMatrix)
-            {
                 newNodeList.Add(new RMatrixNode(rMatrixNode.Type, rMatrixNode.Position, rMatrixNode.Count));
-            }
 
+            // Выполняем необходимое переопределение данных для нового класса
             newRMatrix.rMatrix = newNodeList;
             newRMatrix.typePriority = new List<int>(typePriority);
             newRMatrix.lastPosition = lastPosition;
 
+            // Возвращаем новый экземпляр класса
             return newRMatrix;
         }
 
         /// <summary>
-        /// Находит узел в матрице R в заданной позиции
+        /// Данная функция находит узел в матрице R в заданной позиции
         /// </summary>
-        /// <param name="position"></param>
-        /// <returns></returns>
+        /// <param name="position">Позиция узла по которой необходимо выполнить поиск</param>
+        /// <returns>Узел из матрица R</returns>
         public RMatrixNode Find(int position)
         {
+            // Выполяем поиск узла по позиции
             foreach (RMatrixNode rMatrixNode in rMatrix)
-            {
                 if (rMatrixNode.Position == position)
-                {
                     return rMatrixNode;
-                }
-            }
 
+            // Вовзращаем null в случае отсутствия узла в списке
             return null;
         }
 
         /// <summary>
-        /// Возвращает последнюю позицию в матрице R
+        /// Данная функция возвращает последнюю позицию в матрице R
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Индкс последней позиции в матрице R</returns>
         public int GetLastPosition()
         {
             return lastPosition;
--- newAlgorithm/Model/RMatrix/RMatrixNode.cs
+++ newAlgorithm/Model/RMatrix/RMatrixNode.cs
@@ -1,20 +1,33 @@
 ﻿namespace newAlgorithm.Model
 {
+    
+    /// <summary>
+    /// Определяет множество узлов матрицы R и определяется как количетсво данных типа Type в партии Position
+    /// </summary>
     public class RMatrixNode
     {
-        /// <summary>
-        /// Тип заданий
-        /// </summary>
-        public int Type { get; } = 0;
-        /// <summary>
-        /// Позиция в матрице
-        /// </summary>
-        public int Position { get; } = 0;
-        /// <summary>
-        /// Количество пакетов в текущей позиции
-        /// </summary>
-        public int Count { get; } = 0;
 
+        /// <summary>
+        /// Данная переменная представляет из себя тип задания данного узла
+        /// </summary>
+        public int Type { get; }
+
+        /// <summary>
+        /// Данная переменная представляет из себя позицию в матрице данного узла
+        /// </summary>
+        public int Position { get; }
+
+        /// <summary>
+        ///  Данная переменная содержит в себе количество пакетов в текущей позиции текущего типа данного узла
+        /// </summary>
+        public int Count { get; }
+
+        /// <summary>
+        /// Данный конструктор принимает 3 параметра и возвращает экземпляр класса узла матрицы R
+        /// </summary>
+        /// <param name="type">Определяет тип задания, данного узла</param>
+        /// <param name="position">Определяет позицию задания, данного узла</param>
+        /// <param name="count">Определяет количество данных в данном узле</param>
         public RMatrixNode(int type, int position, int count)
         {
             Type = type;
