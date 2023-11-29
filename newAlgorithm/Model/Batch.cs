ff9e09f40d553ee782d9a4cd1607fd26bb1b5742
Ivan Konovalov <my_world1105@mail.ru>
2023-11-29 17:57:40 +0300

    Add new class Batch with params Size and Type

---
+++ newAlgorithm/Model/Batch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace magisterDiplom.Model
+{
+
+    /// <summary>
+    /// Данный класс описывает партию одного типа данных
+    /// </summary>
+    public class Batch
+    {
+
+        /// <summary>
+        /// Данная переменная описывает тип данных партии
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// Данная переменная описывает количество данных в партии
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Данный конструктор создаём партию типа данных type и размером size
+        /// </summary>
+        /// <param name="type">Тип данных партии</param>
+        /// <param name="size">Размер партии</param>
+        public Batch(int type, int size)
+        {
+            Type = type;
+            Size = size;
+        }
+
+        /// <summary>
+        /// Данная функция выполняет обработку пакета и возрващает время необходимое на его обработку
+        /// </summary>
+        /// <param name="proccessingTime"></param>
+        /// <returns></returns>
+        public int GetTime(int proccessingTime)
+        {
+            return Size * proccessingTime;
+        }
+    }
+}
