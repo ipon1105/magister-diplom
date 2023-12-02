451b9a140cf700f89d6d99174c653c30e11431be
Ivan Konovalov <my_world1105@mail.ru>
2023-12-02 09:43:14 +0300

    Class PreMaintenance was created. Comments was added

---
+++ newAlgorithm/PMoD/PreMaintenance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace magisterDiplom.PMoD
+{
+
+    /// <summary>
+    /// Данный класс реализует весь функционал связаннный с ПТО (Предварительным Технически Обслуживанием) приборов
+    /// </summary>
+    public class PreMaintenance
+    {
+        // Сводка:
+        // Под надёжностью конвейерной системы подразумевается вероятность выполнения возложенных на неё функций в течение заданного интервала времени.
+        //
+        // Обозначения:
+        // dataType(i) - Индекс (идентификатор) типа данных (от 1 до dataTypesCount(n))
+
+        /// <summary>
+        /// Количество типов данных, так же известное, как n;
+        /// </summary>
+        int dataTypesCount;
+
+
+    }
+}
